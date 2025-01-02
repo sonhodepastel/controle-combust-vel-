0f8c4a55fa08effffbee3d3113d4c8786bb866f6
sonhodepastel <160452444+sonhodepastel@users.noreply.github.com>
2025-01-02 19:37:12 -0300

    Delete notification-worker.js

--- notification-worker.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('push', event => {
-    const data = event.data.json();
-    const options = {
-        body: data.body,
-        icon: 'icon-192x192.png',
-        sound: 'notification.mp3'
-    };
-    self.registration.showNotification(data.title, options);
-});
